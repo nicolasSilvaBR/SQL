e2be3a97d445ea9730358db82b6ca3803589fb51
Nicolas Antonio da Silva <78442901+nicolasSilvaBR@users.noreply.github.com>
2021-12-27 12:01:18 +0000

    Add files via upload

---
+++ Lead and Lag.sql
@@ -0,0 +1,41 @@
+/*
+Database = AdventureWorks2019
+
+What is Lead SQL?
+In SQL Server (Transact-SQL), the LEAD function is an analytic function 
+that lets you query more than one row in a table at a time without having 
+to join the table to itself. It returns values from the next row in the table.
+
+What is lead and lag SQL?
+For starters, the LEAD and LAG functions were first introduced in SQL Server 2012. 
+They are window functions. The LEAD function is used to access data from SUBSEQUENT 
+rows along with data from the current row. The LAG function is used to access data 
+from PREVIOUS rows along with data from the current row.
+
+*/
+
+/*
+"Next2OrderByEmployeeVendor" that returns, within the group of all orders 
+that have the same EmployeeID, the vendor name offset TWO orders into the 
+“future” relative to the order in the current row.
+*/
+
+SELECT
+	pp.PurchaseOrderID,
+	pp.OrderDate,
+	pp.TotalDue,
+	pv.Name,
+	pp.VendorID,
+	PrevOrderFromVendorAmt = LAG(pp.TotalDue,1) OVER(partition by pp.VendorID ORDER BY pp.OrderDate),
+	NextOrderByEmployeeVendor = LEAD(pv.Name,1)	OVER(partition by pp.EmployeeID ORDER BY pp.OrderDate ),
+	Next2OrderByEmployeeVendor = LEAD(pv.name)
+
+FROM Purchasing.PurchaseOrderHeader AS pp
+INNER JOIN Purchasing.Vendor as pv
+ON pp.VendorID = pv.BusinessEntityID
+WHERE year(pp.OrderDate) >= 2013
+AND pp.TotalDue >500
+ORDER BY 
+  pp.VendorID,
+  pp.OrderDate
+go
