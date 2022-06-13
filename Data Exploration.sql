d2648d6e6613b015750a24140f95b965a2962400
Nicolas Antonio da Silva <78442901+nicolasSilvaBR@users.noreply.github.com>
2022-06-13 12:55:29 +0100

    Add files via upload

---
+++ Data Exploration.sql
@@ -0,0 +1,26 @@
+-- Data Exploration
+
+SELECT
+    t.NAME AS Entidade,
+    p.rows AS [Rows],
+    SUM(a.total_pages) * 8 AS EspacoTotalKB,
+    SUM(a.used_pages) * 8 AS EspacoUsadoKB,
+    (SUM(a.total_pages) - SUM(a.used_pages)) * 8 AS EspacoNaoUsadoKB
+FROM
+    sys.tables t
+INNER JOIN
+    sys.indexes i ON t.OBJECT_ID = i.object_id
+INNER JOIN
+    sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
+INNER JOIN
+    sys.allocation_units a ON p.partition_id = a.container_id
+LEFT OUTER JOIN
+    sys.schemas s ON t.schema_id = s.schema_id
+WHERE
+    t.NAME NOT LIKE 'dt%'
+    AND t.is_ms_shipped = 0
+    AND i.OBJECT_ID > 255
+GROUP BY
+    t.Name, s.Name, p.Rows
+ORDER BY
+    [Rows] DESC
